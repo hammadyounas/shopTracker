99d255c45cb3be8dea415314920a85972e772300
Hammad Younas <mohsinghani.777@gmail.com>
2017-11-04 08:44:37 -0700

    firebase added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,23 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
+
+//firebase
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule, AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyBKiR5OhwAxPr76PS7mdFh4rTP5PaVmGSc",
+  authDomain: "shoptracker-app.firebaseapp.com",
+  databaseURL: "https://shoptracker-app.firebaseio.com",
+  projectId: "shoptracker-app",
+  storageBucket: "shoptracker-app.appspot.com",
+  messagingSenderId: "770873585528"
+};
 
 @NgModule({
   declarations: [
@@ -14,7 +29,10 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -24,6 +42,7 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
+    AngularFireDatabase,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
